7baaf240ed123107635026dabaabed87bee3994e
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2023-02-21 23:47:22 -0300

    Committing files

---
+++ CodeForces/GNU C++17 (64)/459B | Pashmak and Flowers/186695329.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+ 
+int main(){
+ 
+ long long t, c;
+ cin >> t;
+ long long n[t];
+ 
+ for(int i = 0; i < t; i++){
+  cin >> n[i];
+ }
+ 
+ long long m = *max_element(n, n + t), u = *min_element(n, n + t);
+ 
+ if(t == 2){
+  c = 1;
+ }
+ else if(m == u){
+  c = (pow(count(n, n + t, m), 2) - count(n, n + t, m)) / 2;
+ }
+ else{
+  c = count(n, n + t, m) * count(n, n + t, u);
+ }
+ cout << m - u << " " << c; 
+ 
+return 0;
+}
