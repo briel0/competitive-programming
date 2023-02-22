eeeec0b99dd219b1ba71a8f27b3937a988f6328c
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2023-02-21 23:51:51 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1A | Theatre Square/167817288.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+double n, m, a;
+ 
+cin >> n >> m >> a;
+ 
+cout << (long long) ceil(n/a) *  (long long) ceil(m/a);
+ 
+return 0;
+}
