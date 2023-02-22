cfaea2a697e18981bfc9bc57dea57c23dd5ff6f5
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2023-02-21 23:45:57 -0300

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1777A | Everybody Likes Good Arrays!/190040569.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+bool p(int n){
+ if(n%2 == 0){
+  return true;
+ }
+ return false;
+}
+ 
+int main(){
+ 
+ int t; cin >> t;
+ while(t--){
+  int n; cin >> n;
+  vector<int> y;
+  
+  for(int i = 0; i < n; i++){
+   int h; cin >> h;
+   y.push_back(h);
+  }
+  
+  bool g = true;
+  while(g){
+   bool r = true;
+   for(int i = 0; i < (y.size()-1); i++){
+    if(p(y[i]) == p(y[i+1])){
+     int o = y[i]*y[i+1];
+     y.erase(y.begin()+i);
+     y.erase(y.begin()+i);
+     y.insert(y.begin()+i, o);
+     r = false;
+     break;
+    }
+   }
+   if(r){
+    g = false;
+   }
+  } 
+  cout << n - y.size() << "\n";
+ }
+ 
+return 0;
+}
